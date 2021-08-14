5f6f1464973df6651ac957a2d6e720e953919c91
huybetiii <88918415+huybetiii@users.noreply.github.com>
2021-08-14 12:22:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương ( lấy trên mạng chứ hy ko văn vở :>)',
+    Đằng sau chữ yêu đây là thương .lấy trên mạng chứ hy ko văn vở :>',
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
